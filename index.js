a9423e92ce3dbf7771d0d404eb10cc792f497d3a
wtherockb <forrestwbutler@gmail.com>
2015-11-30 22:23:17 -0500

    liked back anf front

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ $(document).ready(function() {
 		$("#slide_menu_icon").toggle();
 		});
 });
+
+
+function search() {
+
+	var box = document.getElementById('recipes');
+	var keyword = document.getElementById('search-bar').value;
+	box.innerHTML = keyword;
+	//console.log("just set the recieps to " + keyword);
+
+	var xhttp = new XMLHttpRequest();
+	  xhttp.onreadystatechange = function() {
+	    if (xhttp.readyState == 4 && xhttp.status == 200) {
+		 box.innerHTML = xhttp.responseText;
+
+	    }
+	  };
+	 xhttp.open("GET", "https://mampalicious.herokuapp.com/search?title=" + keyword, true);
+	  xhttp.send();
+
+}
