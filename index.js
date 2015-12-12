24fff848348b8a058222f1133e6f2e4a6b6d38ee
Nicholas Yan <nicholasyan11@gmail.com>
2015-12-12 17:16:58 -0500

    add search result styling

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ $(document).ready(function() {
 
 	$("#menu_icon").click(function() {
 
-
-		// console.log('clicked!');
-
 		$(".container-fluid").animate({
 			left: "10%"
 		});	
@@ -29,7 +26,13 @@ $(document).ready(function() {
 /* Sends request to search for recipes by title */
 function search() {
 
-	// console.log("search type is", type);
+	// remove the previous search results
+	// (avoiding removing the recipe_template which we populate)
+	$(".recipe_item").each(function(i, val) {
+		if (val.id != "recipe_template")
+			val.remove();
+	});
+
 	var active = document.getElementsByClassName('active');
 	var type = (active[0].innerHTML).toLowerCase();
 	type = type.trim();
@@ -38,10 +41,40 @@ function search() {
 	var keyword = document.getElementById('search-bar').value;
 	keyword = keyword.trim();
 
+	// query the database
 	var xhttp = new XMLHttpRequest();
-	  xhttp.onreadystatechange = function() {
+  	xhttp.onreadystatechange = function() {
 	    if (xhttp.readyState == 4 && xhttp.status == 200) {
-		 box.innerHTML = xhttp.responseText;
+
+	    	// for each search result returned from the database, append it to our results
+	    	$.each($.parseJSON(xhttp.responseText), function(i, val) {
+
+	    		var title = val.display_title;
+	    		var instructions = val.instructions;
+
+    			// duplicate the template div
+    			var new_recipe = $("#recipe_template").clone();
+
+    			// load the information
+    			new_recipe.find(".title").html(title);
+    			new_recipe.find(".instructions").html("Instructions: " + instructions);
+
+    			var ingredient_list = new_recipe.find(".ingredients");
+	    		$.each(val.ingredients, function(i, ingredient) {
+	    			ingredient_list.append(ingredient + ", " + val.amounts[i] + "<br>");
+	    		});
+
+	    		// update ID
+	    		new_recipe.attr("id", title);
+
+    			// remove hidden designation
+    			new_recipe.removeClass("hidden");
+
+    			// add it to our recipe list
+    			new_recipe.appendTo("#recipes");
+
+	    	});
+
 	    }
 	  };
 	 xhttp.open("GET", "https://mampalicious.herokuapp.com/searchtitle?type="+type+"&keyword="+ keyword.toLowerCase(), true);
