5820586d8a4241366c8d20ff74dbe309bb61ca3d
Jade <jadeyychan@gmail.com>
2016-02-24 22:12:14 -0500

    angular reconstruction get request not functioning

--- index.js
+++ index.js
@@ -1,85 +1,110 @@
-$(document).ready(function() {
-	$("#slide_menu_icon").hide();
+var RecipesApp=angular.module("Recipes", []);
 
-	$("#menu_icon").click(function() {
-		$("#slide_menu_icon").toggle();
-	});
+RecipesApp.controller("RecipesController", function($scope, $http){
+	console.log("inside recipes controller");
 
-	$(".btn").click(function(e) {
-		var target = $(e.target), type;
-		if (target.is('#title')) {
-			document.getElementById("title").setAttribute('class','btn btn-default active');
-			document.getElementById("ingred").setAttribute('class','btn btn-default');
-		} else if (target.is('#ingred')) {
-			document.getElementById("title").setAttribute('class','btn btn-default');
-			document.getElementById("ingred").setAttribute('class','btn btn-default active');
-		}
-	});
+	$scope.search=function() {
+		console.log("inside search function");
+		var type = "ingredients";
+		var keyword = "tahini";
+
+		// GET Request 
+		$http({
+			method: 'GET',
+			url: "https://mampalicious.herokuapp.com/searchtitle?type="+type+"&keyword="+ keyword.toLowerCase()+"&keywordOriginal=" + keyword.toLowerCase()
+		}).then(function success(response) {
+			console.log("SUCCESS");
+		}, function error(response) {
+			console.log("ERROR");
+		});
+	};
 });
 
+// $(document).ready(function() {
+// 	$(".dropdown").on("click", function(event) {
+// 		$(event.target).closest(".dropdown").siblings(".more_info").toggleClass("hidden");
+// 	});
+// 	$("#slide_menu_icon").hide();
 
-/* Sends request to search for recipes by title */
-function search() {
+// 	$("#menu_icon").click(function() {
+// 		$("#slide_menu_icon").toggle();
+// 	});
 
-	// remove the previous search results
-	// (avoiding removing the recipe_template which we populate)
-	$(".recipe_item").each(function(i, val) {
-		if (val.id != "recipe_template")
-			val.remove();
-	});
+// 	$(".btn").click(function(e) {
+// 		var target = $(e.target), type;
+// 		if (target.is('#title')) {
+// 			document.getElementById("title").setAttribute('class','btn btn-default active');
+// 			document.getElementById("ingred").setAttribute('class','btn btn-default');
+// 		} else if (target.is('#ingred')) {
+// 			document.getElementById("title").setAttribute('class','btn btn-default');
+// 			document.getElementById("ingred").setAttribute('class','btn btn-default active');
+// 		}
+// 	});
+// });
 
-	var active = document.getElementsByClassName('active');
-	var type = (active[0].innerHTML).toLowerCase();
-	type = type.trim();
 
-	var box = document.getElementById('recipes');
-	var keyword = document.getElementById('search-bar').value;
-	keyword = keyword.trim();
+// /* Sends request to search for recipes by title */
+// function search() {
 
-	// query the database
-	var xhttp = new XMLHttpRequest();
-  	xhttp.onreadystatechange = function() {
-	    if (xhttp.readyState == 4 && xhttp.status == 200) {
+// 	// remove the previous search results
+// 	// (avoiding removing the recipe_template which we populate)
+// 	$(".recipe_item").each(function(i, val) {
+// 		if (val.id != "recipe_template")
+// 			val.remove();
+// 	});
 
-	    	// for each search result returned from the database, append it to our results
-	    	$.each($.parseJSON(xhttp.responseText), function(i, val) {
+// 	var active = document.getElementsByClassName('active');
+// 	var type = (active[0].innerHTML).toLowerCase();
+// 	type = type.trim();
 
-	    		var title = val.display_title;
-	    		var instructions = val.instructions;
+// 	var box = document.getElementById('recipes');
+// 	var keyword = document.getElementById('search-bar').value;
+// 	keyword = keyword.trim();
 
-    			// duplicate the template div
-    			var new_recipe = $("#recipe_template").clone();
+// 	// query the database
+// 	var xhttp = new XMLHttpRequest();
+//   	xhttp.onreadystatechange = function() {
+// 	    if (xhttp.readyState == 4 && xhttp.status == 200) {
 
-    			// load the information
-    			new_recipe.find(".title").html(title);
-    			new_recipe.find(".instructions").html("Instructions: " + instructions);
+// 	    	// for each search result returned from the database, append it to our results
+// 	    	$.each($.parseJSON(xhttp.responseText), function(i, val) {
 
-    			var ingredient_list = new_recipe.find(".ingredients");
-	    		$.each(val.display_ingredients, function(i, ingredient) {
-	    			ingredient_list.append(ingredient + ", " + val.amounts[i] + "<br>");
-	    		});
+// 	    		var title = val.display_title;
+// 	    		var instructions = val.instructions;
 
-	    		// update ID
-	    		new_recipe.attr("id", title);
+//     			// duplicate the template div
+//     			var new_recipe = $("#recipe_template").clone();
 
-    			// remove hidden designation
-    			new_recipe.removeClass("hidden");
+//     			// load the information
+//     			new_recipe.find(".title").html(title);
+//     			new_recipe.find(".instructions").html("Instructions: " + instructions);
 
-    			// add it to our recipe list
-    			new_recipe.appendTo("#recipes");
+//     			var ingredient_list = new_recipe.find(".ingredients");
+// 	    		$.each(val.display_ingredients, function(i, ingredient) {
+// 	    			ingredient_list.append(ingredient + ", " + val.amounts[i] + "<br>");
+// 	    		});
 
-	    	});
+// 	    		// update ID
+// 	    		new_recipe.attr("id", title);
 
-	    }
-	  };
-	 xhttp.open("GET", "https://mampalicious.herokuapp.com/searchtitle?type="+type+"&keyword="+ keyword.toLowerCase()+"&keywordOriginal=" + keyword.toLowerCase(), true);
-	 xhttp.send();
+//     			// remove hidden designation
+//     			new_recipe.removeClass("hidden");
 
-}
+//     			// add it to our recipe list
+//     			new_recipe.appendTo("#recipes");
 
-/* Handles the event of a enter-bar initiated search */
-function keypress_search(event) {
-	if(event.keyCode == 13){
-		search();
-	}
-}
+// 	    	});
+
+// 	    }
+// 	  };
+// 	 xhttp.open("GET", "https://mampalicious.herokuapp.com/searchtitle?type="+type+"&keyword="+ keyword.toLowerCase()+"&keywordOriginal=" + keyword.toLowerCase(), true);
+// 	 xhttp.send();
+
+// }
+
+// /* Handles the event of a enter-bar initiated search */
+// function keypress_search(event) {
+// 	if(event.keyCode == 13){
+// 		search();
+// 	}
+// }
